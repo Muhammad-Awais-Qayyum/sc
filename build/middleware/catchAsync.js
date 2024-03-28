b008483208aa3e88f942fac1406fc7acb1690baa
Muhammad Awais Qayyum <awaisquyyum010@gmail.com>
2024-03-28 12:32:08 -0700

    Deploy

--- build/middleware/catchAsync.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CatchAsync = void 0;
-const CatchAsync = (func) => (req, res, next) => {
-    Promise.resolve(func(req, res, next)).catch(next);
-};
-exports.CatchAsync = CatchAsync;
