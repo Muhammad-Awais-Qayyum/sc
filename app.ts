565d6d6ff5e69d2ec8ee4548fda2b95dabd6945c
Muhammad Awais Qayyum <awaisquyyum010@gmail.com>
2024-03-28 14:12:40 -0700

    first commit

--- app.ts
+++ app.ts
@@ -39,12 +39,9 @@ app.use(
 
 
 const limiter = rateLimit({
-	windowMs: 15 * 60 * 1000, // 15 minutes
-	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
-	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
-	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
-	// store: ... , // Redis, Memcached, etc. See below.
-})
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  });
 
 // Apply the rate limiting middleware to all requests.
 app.use(limiter)
